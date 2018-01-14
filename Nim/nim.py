91921dfe6f0fec6cc31aa896d9807709bc3bed66
Richard Dalton <richard@devjoy.com>
2018-01-14 12:51:12 +0000

    Updated Nim to Python 3

--- Nim/nim.py
+++ Nim/nim.py
@@ -6,25 +6,25 @@ heaps = {'A': randint(3, 9),
 
 
 def Display_Heaps():
-    print "The heaps are:"
+    print ("The heaps are:")
     for heap, count in sorted(heaps.items()):
-        print "In heap {0}, there are {1}".format(heap, count)
+        print ("In heap {0}, there are {1}".format(heap, count))
 
 def Get_Move_From_Player():
     while True:
         Display_Heaps()
-        heap = raw_input("It's {0}'s turn. Which heap do you want to take from? : ".format(players[player]))
+        heap = input("It's {0}'s turn. Which heap do you want to take from? : ".format(players[player]))
         if not heap in heaps:
-            print "There is no heap called {0}".format(heap)
+            print ("There is no heap called {0}".format(heap))
         else:
-            how_many = input("How many would you like to take from heap {0}? : ".format(heap))
+            how_many = int(input("How many would you like to take from heap {0}? : ".format(heap)))
             if how_many > heaps[heap]:
-                print "There aren't {0} pieces in heap {1}".format(how_many, heap)
+                print ("There aren't {0} pieces in heap {1}".format(how_many, heap))
             else:
                 return heap, how_many
 
-player1 = raw_input("Enter Player One's Name: ")
-player2 = raw_input("Enter Player Two's Name: ")
+player1 = input("Enter Player One's Name: ")
+player2 = input("Enter Player Two's Name: ")
 players = [player1, player2]
 
 
@@ -45,4 +45,4 @@ while not game_over:
     player = player % 2
 
 
-print "{0} has taken the last piece and is the winner".format(players[player])
+print ("{0} has taken the last piece and is the winner".format(players[player]))
