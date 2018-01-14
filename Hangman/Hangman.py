e48b2a872b7bba0e4e8aaed0239087ddd57ce1a3
Richard Dalton <richard@devjoy.com>
2018-01-14 12:43:48 +0000

    Make Hangman read from dictionary

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,16 +1,20 @@
 from random import choice
 
-word = choice(["MONOPOLY", "FRUIT", "AUTOMOBILE", "SPORT"])
+with open("/usr/share/dict/words") as f:
+    words = list(set([word.lower() for word in f.read().split('\n') if len(word) > 3]))
+
+
+word = choice(words)
 unguessed = set(word)
 lives = 5
 
 while unguessed and (lives > 0):
-    print "You have {0} lives left.".format(lives)
+    print("You have {0} lives left.".format(lives))
 
     word_clue = ' '.join(['*' if c in unguessed else c for c in word])
-    print "The word is: {0}".format(word_clue)
+    print("The word is: {0}".format(word_clue))
 
-    guess = raw_input("Guess a letter: ")
+    guess = input("Guess a letter: ")
 
     if guess in unguessed:
         unguessed.remove(guess)
@@ -18,7 +22,7 @@ while unguessed and (lives > 0):
         lives -= 1
 
 if not unguessed:
-    print "You got it, the word was {0}".format(word)
+    print("You got it, the word was {0}".format(word))
 else:
-    print "You didn't get it, the word was {0}".format(word)
+    print("You didn't get it, the word was {0}".format(word))
 
