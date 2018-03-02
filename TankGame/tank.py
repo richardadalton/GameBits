70bb900c084c8e6cbdfc59ae627ab6a5ab4517f4
Richard Dalton <richard@devjoy.com>
2018-03-02 23:05:17 +0000

    Upgrade to Python3

--- TankGame/tank.py
+++ TankGame/tank.py
@@ -14,18 +14,18 @@ class Tank(object):
     def fire_at(self, enemy):
         if self.ammo >= 1:
             self.ammo -= 1
-            print self.name, "fires on", enemy.name
+            print (self.name, "fires on", enemy.name)
             enemy.hit()
         else:
-            print self.name, 'Has no shells!'
+            print (self.name, 'Has no shells!')
 
     def hit(self):
         self.armour -= 20
-        print self.name, "is hit!"
+        print (self.name, "is hit!")
         if self.armour <= 0:
             self.explode()
 
     def explode(self):
         self.alive = False
-        print self.name, 'explodes!'
+        print (self.name, 'explodes!')
 
--- TankGame/tankgame.py
+++ TankGame/tankgame.py
@@ -6,34 +6,34 @@ tanks = {'a': Tank('Darragh'), 'b': Tank('Bob'), 'c': Tank('Dave')}
 alive_tanks = len(tanks)
 
 while alive_tanks > 1:
-    print
+    print ()
     for tank_name in sorted(tanks.keys()):
-        print tank_name, tanks[tank_name]
+        print (tank_name, tanks[tank_name])
 
-    first = raw_input('Who fires? ').lower()
-    second = raw_input('Who do they fire at? ').lower()
+    first = input('Who fires? ').lower()
+    second = input('Who do they fire at? ').lower()
 
     try:
         first_tank = tanks[first]
         second_tank = tanks[second]
-    except KeyError, name:
-        print 'No tank named ', name
+    except (KeyError, name):
+        print ('No tank named ', name)
         continue
 
     if not first_tank.alive or not second_tank.alive:
-        print "One of those tanks are dead!"
+        print ("One of those tanks are dead!")
         continue
 
-    print
-    print "*" * 30
+    print ()
+    print ("*" * 30)
 
     first_tank.fire_at(second_tank)
     if not second_tank.alive:
         alive_tanks -= 1
 
-    print '*' * 30
+    print ('*' * 30)
 
 for tank in tanks.values():
     if tank.alive:
-        print tank.name, 'is the winner!'
+        print (tank.name, 'is the winner!')
         break
